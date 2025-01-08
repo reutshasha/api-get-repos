61dca8871b7bfb7b78c5884ae62f56589cb90c44
reutshasha <reutsben@gmail.com>
2025-01-08 14:05:45 +0200

    Add files via upload

---
+++ BL/Interfaces/IFavoritesService.cs
@@ -0,0 +1,12 @@
+﻿using Shared.Models;
+
+namespace BL.Interfaces
+{
+    public interface IFavoriteService
+    {
+        Task AddFavoriteAsync(GitHubRepository favorite);
+        Task<List<GitHubRepository>> GetFavoriteByIdAsync(string userId);
+        Task<List<GitHubRepository>> GetFavoritesAsync();
+
+    }
+}
---
+++ BL/Services/FavoritesService.cs
@@ -0,0 +1,47 @@
+﻿using BL.Interfaces;
+using DAL.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace BL.Services
+{
+    public class FavoriteService : IFavoriteService
+    {
+        private readonly FavoriteDbContext _context;
+
+        public FavoriteService(FavoriteDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddFavoriteAsync(GitHubRepository favorite)
+        {
+            var exists = await _context.Favorite.AnyAsync(f => f.Id == favorite.Id);
+            if (!exists)
+            {
+                try
+                {
+                    _context.Favorite.Add(favorite);
+                    await _context.SaveChangesAsync();
+                }
+                catch (Exception ex)
+                {
+
+                    return;//TODO
+                }
+
+            }
+        }
+
+        public async Task<List<GitHubRepository>> GetFavoriteByIdAsync(string id)
+        {
+            return await _context.Favorite
+                                 .ToListAsync();
+        }
+        public async Task<List<GitHubRepository>> GetFavoritesAsync()
+        {
+            return await _context.Favorite
+                                 .ToListAsync();
+        }
+    }
+}
