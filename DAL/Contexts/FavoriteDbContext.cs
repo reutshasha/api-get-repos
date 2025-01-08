c9a24fd55ae94c5d744b9453de401d886ca78b00
reutshasha <reutsben@gmail.com>
2025-01-08 14:07:10 +0200

    Add files via upload

---
+++ DAL/Contexts/FavoriteDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace DAL.Contexts
+{
+    public class FavoriteDbContext : DbContext
+    {
+        public FavoriteDbContext(DbContextOptions<FavoriteDbContext> options) : base(options) { }
+        public DbSet<GitHubRepository> Favorite { get; set; }
+
+    }
+}
