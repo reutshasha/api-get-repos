c0a830d3d0d3ff59d21752e789fa87f62f1a1775
reutshasha <hemoreut@gmail.com>
2025-01-08 13:57:05 +0200

    init

---
+++ Controllers/RepositoriesController.cs
@@ -0,0 +1,44 @@
+﻿using BL.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitRepositoriesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RepositoriesController : ControllerBase
+    {
+        private readonly IGitHubService _gitHubService;
+        private readonly IEmailSender _emailSender;
+
+        public RepositoriesController(IGitHubService gitHubService, IEmailSender emailSender)
+        {
+            _gitHubService = gitHubService;
+            _emailSender = emailSender;
+        }
+
+        [Authorize]
+        [HttpGet("SearchRepositories")]
+        public async Task<IActionResult> SearchRepositories(
+        [FromQuery] string query,
+        [FromQuery] int page = 1,
+        [FromQuery] int perPage = 20)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                return BadRequest("The 'query' parameter is required.");
+            }
+
+            try
+            {
+                var result = await _gitHubService.SearchRepositoriesAsync(query, page, perPage);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
+        }
+
+    }
+}
